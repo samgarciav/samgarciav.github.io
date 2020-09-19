237438c8ec4bd36cdebd30875e4796a70322c24a
samgarciav <samgraciav@gmail.com>
2020-09-19 12:37:36 -0500

    using lastModified

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,3 +1,4 @@
+/*-- First attempt -- 
 let dd =  new Date().getDate();
 let mm =  new Date().getMonth() +1; 
 let yyy = new Date().getFullYear();
@@ -8,4 +9,7 @@ let sec = new Date().getSeconds();
 
 let formated_day_time = mm+'/'+dd+'/'+yyy+' '+ hrs+':'+min+':'+sec;
 
-document.getElementById('lastUpdated').innerHTML = formated_day_time;
+document.getElementById('lastModified').innerHTML = formated_day_time;
+*/
+
+document.getElementById('lastModified').innerHTML = document.lastModified;
