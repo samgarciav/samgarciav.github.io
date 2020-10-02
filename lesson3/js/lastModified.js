abc6fe045fe79f04cc6ee67b0b6d7ebf960bb542
samgarciav <samgraciav@gmail.com>
2020-10-02 12:17:39 -0500

    Adding a lot to the weathersite

---
+++ lesson3/js/lastModified.js
@@ -0,0 +1,18 @@
+/*-- First attempt -- 
+let dd =  new Date().getDate();
+let mm =  new Date().getMonth() +1; 
+let yyy = new Date().getFullYear();
+
+let hrs =  new Date().getHours();
+let min =  new Date().getMinutes(); 
+let sec = new Date().getSeconds();
+
+let formated_day_time = mm+'/'+dd+'/'+yyy+' '+ hrs+':'+min+':'+sec;
+
+document.getElementById('lastModified').innerHTML = formated_day_time;
+*/
+
+document.getElementById('lastModified').innerHTML = document.lastModified;
+
+let YYYY = new Date().getFullYear();
+document.getElementById('getFullYear').innerHTML = YYYY;
