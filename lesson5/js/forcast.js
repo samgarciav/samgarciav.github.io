f0bfca5d34ae7f4e6f87aa7d5aba11b0652a7cec
samgarciav <samgraciav@gmail.com>
2020-10-16 10:29:43 -0500

    weather summary

--- lesson5/js/forcast.js
+++ lesson5/js/forcast.js
@@ -1,5 +1,5 @@
-let todays= new Date();
-let tomorrow = todays + 1;
-document.getElementsByClassName("day")[0].innerHTML=todays;
-document.getElementsByClassName("day")[1].innerHTML=tomorrow;
-
+/* const d = new Date();
+let t = new Date(d.getDay() + 1);
+document.getElementsByClassName("day")[0].innerHTML= d;
+document.getElementsByClassName("day")[1].innerHTML= t;
+*/
