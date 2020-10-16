1f4c43b67eeb061fd04b6063f174d421e9f478cf
samgarciav <samgraciav@gmail.com>
2020-10-16 07:18:49 -0500

    pancakes

---
+++ lesson5/js/pancakes.js
@@ -0,0 +1,14 @@
+pancakes();
+
+function pancakes(){
+  if(new Date().getDay() == 5){
+    document.getElementsByTagName("section")[0].style.display="block";
+  }
+  else{
+    console.log("No pancakes today");
+  }
+}
+
+function closepancakes(){
+  document.getElementsByTagName("section")[0].style.display="none";
+}
