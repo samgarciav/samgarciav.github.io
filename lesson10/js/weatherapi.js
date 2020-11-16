3871f2912883e88f8dc1e27fa3d8732dde1c1f2d
samgarciav <samgraciav@gmail.com>
2020-11-16 08:02:18 -0600

    .

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -11,5 +11,4 @@ fetch(apiURL)
     document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
     document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
     document.getElementById('icon').setAttribute('alt', desc);
-
   });
